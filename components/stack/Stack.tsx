fb7ebe1198dffb5cb9676e9d34fd3ce6bde3028d
Ayomikuolatunji <Ayomikuolatunji@gmail.com>
2022-05-30 17:07:09 +0100

    done with contact page

---
+++ components/stack/Stack.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Stack:React.FC = () => {
+  return (
+    <div>
+        <h1>Stacks</h1>
+    </div>
+  )
+}
+
+export default Stack
--- layouts/home/Home.tsx
+++ layouts/home/Home.tsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import AboutMe from '../../components/about-me/AboutMe'
 import Contact from '../../components/contacts/Contacts'
+import Stack from '../../components/stack/Stack'
 
 const Home = () => {
   return (
     <div>
       <AboutMe/>
+      <Stack/>
       <Contact/>
     </div>
   )
