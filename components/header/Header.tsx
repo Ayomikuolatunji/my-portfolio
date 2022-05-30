67403bdf38b268f4a0b43f21fc85f66ee3e3d616
Ayomikuolatunji <Ayomikuolatunji@gmail.com>
2022-05-30 13:24:02 +0100

    header components wrapped around _ app component

--- components/header/Header.tsx
+++ components/header/Header.tsx
@@ -1,9 +1,23 @@
 import React from 'react'
 
 const Header = () => {
+
+  const navItems=[
+    {
+      id:Math.random(),
+      item:"Home"
+    }
+  ]
+
   return (
-    <div>
-      
+    <div className='w-[80%] mx-auto sm:py-10 sm:px-5 '>
+         <div className="logo">
+             <h1>Ayoscript</h1>
+         </div>
+
+         <div className="navtItems">
+
+         </div>
     </div>
   )
 }
