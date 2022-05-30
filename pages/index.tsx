1e33ff5121e869c034b08f8bb6e8ef1edccddb5d
Ayomikuolatunji <Ayomikuolatunji@gmail.com>
2022-05-30 13:52:11 +0100

    working web page header

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,10 @@
 import type { NextPage } from 'next'
-import { motion } from "framer-motion"
-import Image from 'next/image'
+import HomePage from '../layouts/home/Home'
 
 const Home: NextPage = () => {
   return (
     <div className="flex min-h-screen flex-col items-center justify-center py-2">
-       
-      
+       <HomePage/>
     </div>
   )
 }
