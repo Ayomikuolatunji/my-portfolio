8c1dd66a01f6f81048c40632e3716e19185f3af9
Ayomikuolatunji <Ayomikuolatunji@gmail.com>
2022-06-02 21:22:44 +0100

    added travel advisor to projects list

--- components/homePageProjects/Projects.tsx
+++ components/homePageProjects/Projects.tsx
@@ -1,18 +1,20 @@
 import React from 'react'
 import NasaProject from './NasaProject';
+import TravelAdvisor from './TravelAdvisor';
 
 
 
 const Projects:React.FC = () => {
   return (
-    <div className='sm:w-[60%] mx-auto w-[95%] mt-10'>
+    <div className='sm:w-[70%] mx-auto w-[95%] mt-10'>
          <div className="intro flex justify-center items-center">
              <h1 className='text-green-500 text-xl font-extrabold 
              opacity-[1]'>Things I've Built Personally</h1>
              <span className='bg-green-500 h-[5px] w-[57px] block ml-3'></span>
         </div>
-        <div className="projects mt-16 grid sm:grid-cols-3 w-full grid-cols-1">
+        <div className="projects mt-16 grid sm:grid-cols-3 w-full grid-cols-1 gap-3">
                <NasaProject/>
+               <TravelAdvisor/>
         </div>
     </div>
   )
--- components/homePageProjects/TravelAdvisor.tsx
+++ components/homePageProjects/TravelAdvisor.tsx
@@ -2,40 +2,40 @@ import React from 'react'
 import { AiFillGithub } from 'react-icons/ai'
 import { FiExternalLink } from 'react-icons/fi'
 
-const NasaProject:React.FC = () => {
+const TravelAdvisor:React.FC = () => {
   return (
     <div className="project-box w-full shadow-[1px 2px 10px rgba(0,0,0,0.5)]">
             <div className="title mb-3">
                 <span className='text-green-500 font-extrabold my-7 text-2xl'>Featured Project</span>
                     <div className="inner flex items-center justify-between">
-                    <h1>01 Nasa SpaceX Booking Flight</h1> 
+                    <h1>02 Travel Advisor</h1> 
                     <div className="icons flex items-center">
                     <a href="https://github.com/Ayomikuolatunji/Nasa-travel-space-booking" target="_blank">
                     <AiFillGithub className='text-3xl mr-3'/>
                     </a>
-                    <a href="https://nasa-spacex-launch.herokuapp.com/" target="_blank">
+                    <a href="https://travel-map-advisor.netlify.app/" target="_blank">
                     <FiExternalLink className='text-3xl'/>
                     </a>
                     </div>
                 </div>
             </div>
             <div className="nasa ">
-                <img src="/assests/nasa.png" alt="" />
+                <img src="/assests/travel.png" alt="" />
             </div>
             <div className="project-details">
                 <div className="project-desc z-50  bg-[rgba(0,0,0,0.6)] p-3 rounded-b-lg">
                     <p>
-                        This project aim at booking a flight for the spacex. Users can book a flight for the spacex, see history of the flight and cancel the flight. They can also see the status of the flight.All the successfull booked flight will be saved in the database.
+                      I decided to build an application that can be helpful to travelers to locate Hotels, Resturants and Dinings around their current location. The web app is fully responsive to serve users access mode(desktop, tablets and mobile devices).
                     </p>
                 </div>
                 <div className="stacks flex flex-wrap">
-                    <span className='bg-[rgba(0,0,0,0.3)] px-3 py-2 rounded-lg mx-2 mt-2'>Nodejs</span>
-                    <span className='bg-[rgba(0,0,0,0.3)] px-3 py-2 rounded-lg mx-2 mt-2'>Expressjs</span>
-                    <span className='bg-[rgba(0,0,0,0.3)] px-3 py-2 rounded-lg mx-2 mt-2'>MongoDB</span>
-                    <span className='bg-[rgba(0,0,0,0.3)] px-3 py-2 rounded-lg mx-2 mt-2'>Mongoose</span>
                     <span className='bg-[rgba(0,0,0,0.3)] px-3 py-2 rounded-lg mx-2 mt-2'>Reactjs</span>
-                    <span className='bg-[rgba(0,0,0,0.3)] px-3 py-2 rounded-lg mx-3 mt-2'>Arwes</span>
-                    <span className='bg-[rgba(0,0,0,0.3)] px-3 py-2 rounded-lg mx-3 mt-2'>Heroku</span>
+                    <span className='bg-[rgba(0,0,0,0.3)] px-3 py-2 rounded-lg mx-2 mt-2'>Material-Ui</span>
+                    <span className='bg-[rgba(0,0,0,0.3)] px-3 py-2 rounded-lg mx-2 mt-2'>React Icons</span>
+                    <span className='bg-[rgba(0,0,0,0.3)] px-3 py-2 rounded-lg mx-2 mt-2'>@google-map-react</span>
+                    <span className='bg-[rgba(0,0,0,0.3)] px-3 py-2 rounded-lg mx-2 mt-2'>react-google-maps</span>
+                    <span className='bg-[rgba(0,0,0,0.3)] px-3 py-2 rounded-lg mx-3 mt-2'>Tailwindcss</span>
+                    <span className='bg-[rgba(0,0,0,0.3)] px-3 py-2 rounded-lg mx-3 mt-2'>Netlify</span>
                     <span className='bg-[rgba(0,0,0,0.3)] px-3 py-2 rounded-lg mx-3 mt-2'>Jest</span>
                     </div>
             </div>  
@@ -43,4 +43,4 @@ const NasaProject:React.FC = () => {
   )
 }
 
-export default NasaProject
+export default TravelAdvisor
