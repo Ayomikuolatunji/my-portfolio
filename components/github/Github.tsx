fd18f86d95ac5fec777ad7f3bdf289fe1e81be3f
Ayomikuolatunji <Ayomikuolatunji@gmail.com>
2022-06-07 13:12:37 +0100

    I am working on the github page

--- components/github/Github.tsx
+++ components/github/Github.tsx
@@ -1,37 +1,76 @@
 import React, { useEffect, useState } from 'react'
+import { MdSupervisorAccount } from 'react-icons/md'
 
 interface profileDatas{
   avatar_url:string
   name:string
+  bio:string
+  following:string
+  followers:string
 }
 
+interface repo{
+  name:string
+}
 
+interface profileRepos{
+    repos?:[repo]
+}
 
 const Github = () => {
-  const [profileData,setProfileData]=useState()
+  const [profileData,setProfileData]=useState<profileDatas>()
+  const [repos,setRepos]=useState([])
 
   useEffect(()=>{
-     try {
-      const fetchProfile=async()=>{
-        const res=await axios('https://api.github.com/users/Ayomikuolatunji')
+       const fetchProfile=async()=>{
+        const res=await fetch('https://api.github.com/users/Ayomikuolatunji')
         const data=await res.json()
+        console.log(data);
         setProfileData(data)
        }
-     } catch (error) {
-         console.log(error)
-     }
+       fetchProfile()
  },[])
 
+ useEffect(()=>{
+  const fetchRepos=async()=>{
+    const res=await fetch('https://api.github.com/users/Ayomikuolatunji/repos?created="sort=created&direction=asc')
+    const data=await res.json()
+    console.log(data);
+     setRepos(data)
+   }
+   fetchRepos()
+ },[])
+   
   
   return (
-    <div className='w-[70%] mx-auto'>
-          <div className="profile">
+    <div className='sm:w-[70%] w-[95%] mx-auto flex justify-between flex-wrap'>
+          <div className="profile sm:w-[40%]">
               <div className="profile-image">
-                 <h1>{profileData.name}</h1>
+                 <img src={profileData?.avatar_url} alt="img" className='rounded-full w-[300px]'/>
+              </div>
+              <div className="profile-info">
+                  <h1 className='text-white text-5xl'>{profileData?.name}</h1>
+              </div>
+              <div className="bio">
+                  <p>{profileData?.bio}</p>
+              </div>
+              <div className="followers flex flex-row w-full items-center">
+                 <p className='flex items-center mr-4'> <MdSupervisorAccount/> {profileData?.followers} followers</p>
+                 <p className='space-x-3'>{profileData?.following} following</p>
               </div>
           </div>
-          <div className="repos">
-
+          <div className="repos sm:w-[60%] w-full mt-10">
+               {
+                 repos.map((repo:repo,index)=>{
+                   return (
+                     <div key={index} className="border-b-3">
+                        <div className="repo-box">
+                            <h1>{repo.name}</h1>
+                        </div>
+                     </div>
+                   )
+                 })
+               }
           </div>
     </div>
   )
