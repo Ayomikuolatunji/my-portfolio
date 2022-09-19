a008e23c5118fb84c388a582c0bd68b8ad8b017c
Ayomikuolatunji <Ayomikuolatunji@gmail.com>
2022-09-19 21:01:23 +0100

    updating github project

--- components/github/Github.tsx
+++ components/github/Github.tsx
@@ -26,7 +26,7 @@ const Github = () => {
   const [repos, setRepos] = useState([])
 
   useEffect(() => {
-    try{
+    try {
       const fetchProfile = async () => {
         const res = await fetch('https://api.github.com/users/Ayomikuolatunji')
         const data = await res.json()
@@ -34,24 +34,24 @@ const Github = () => {
         setProfileData(data)
       }
       fetchProfile()
-    }catch(err){
-        console.log(err)
+    } catch (err) {
+      console.log(err)
     }
   }, [])
 
   useEffect(() => {
-     try {
+    try {
       const fetchRepos = async () => {
         const res = await fetch(
           'https://api.github.com/users/Ayomikuolatunji/repos?sort="updated_at"&created&direction=desc'
         )
         const data = await res.json()
-        setRepos(data.slice())
+        setRepos(data)
       }
       fetchRepos()
-     } catch (error) {
-        console.log(error);
-     }
+    } catch (error) {
+      console.log(error)
+    }
   }, [])
 
   return (
@@ -88,35 +88,34 @@ const Github = () => {
           </h1>
           <span className="bg-green-500 h-[5px] w-[57px] block ml-3"></span>
         </div>
-        {repos.map((repo: repo, index) => {
-          return (
-            <div key={index} className="border-b-2 py-4 border-gray">
-              <div className="repo-box">
-                <h1 className="text-white font-extrabold text-3xl flex items-center">
-                  <a href={repo.svn_url} target="_blank">
-                    {repo.name}
-                  </a>
-                  <span className="text-sm ml-3 border-2 border-gray-200 py-1 px-2 rounded-3xl">
-                    {repo.visibility}
+        <div className="sm:w-0 w-full mx-auto">
+          {(repos || []).map((repo: repo, index) => {
+            return (
+              <div key={index} className="border-b-2 py-4 border-gray">
+                <div className="repo-box">
+                  <h1 className="text-white font-extrabold sm:text-3xl text-2xl flex items-center">
+                    <a href={repo.svn_url} target="_blank">
+                      {repo.name}
+                    </a>
+                  </h1>
+                </div>
+                <div className="desc">
+                  <p>{repo.description}</p>
+                </div>
+                <div className="language my-3">
+                  <span className="py-1 px-4 text-yellow-400 border-2 rounded-3xl border-white">
+                    {repo.language || 'Javascript'}
                   </span>
-                </h1>
+                </div>
+                {repo?.updated_at && (
+                  <span className="font-bold">
+                    Updated {moment(repo?.updated_at).fromNow() ?? ''}
+                  </span>
+                )}
               </div>
-              <div className="desc">
-                <p>{repo.description}</p>
-              </div>
-              <div className="language my-3">
-                <span className="py-1 px-4 text-yellow-400 border-2 rounded-3xl border-white">
-                  {repo.language}
-                </span>
-              </div>
-              {repo?.updated_at && (
-                <span className="font-bold">
-                  Updated {moment(repo?.updated_at).fromNow() ?? ''}
-                </span>
-              )}
-            </div>
-          )
-        })}
+            )
+          })}
+        </div>
       </div>
     </div>
   )
